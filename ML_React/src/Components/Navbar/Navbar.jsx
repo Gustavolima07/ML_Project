e6c64d808c82e1a8e1082b38248582e9904a3288
Gustavo_lima07 <161834248+Gustavolima07@users.noreply.github.com>
2025-05-16 10:13:11 -0300

    Navbar criado

---
+++ ML_React/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+
+const Navbar = () => {
+    return (
+    <header class="bg-zinc-700">
+        <nav class="flex justify-between items-center w-[92%]  mx-auto text-2xl py-7">
+            <div>
+                <img class="w-10 cursor-pointer" src="https://cdn4.iconfinder.com/data/icons/weather-conditions/512/lightning-512.png" alt="..." />
+            </div>
+            <div class="nav-links duration-500 md:static absolute
+            md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
+                <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 text-amber-50">
+                    <li>
+                        <a className="hover:text-gray-500 " href="#">Products</a>
+                    </li>
+                    <li>
+                        <a className="hover:text-gray-500" href="#">Solution</a>
+                    </li>
+                    <li>
+                        <a className="hover:text-gray-500" href="#">Resource</a>
+                    </li>
+                    <li>
+                        <a className="hover:text-gray-500" href="#">Developers</a>
+                    </li>
+                    <li>
+                        <a className="hover:text-gray-500" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex items-center gap-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 cursor-pointer">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 
+                            0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+                <span className="cursor-pointer">Profile</span>    
+
+            </div>
+        </nav>
+    </header>
+    );
+};
+
+export { Navbar };
